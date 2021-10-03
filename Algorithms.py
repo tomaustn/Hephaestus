f33e74b87f3eb44a8e83f5895f6bc6704e42ab1c
Alreaii <77882565+Alreaii@users.noreply.github.com>
2021-10-03 23:44:32 +0100

    Update Algorithms.py

--- Algorithms.py
+++ Algorithms.py
@@ -70,23 +70,6 @@ class DangerRating:
             print(DangerRating.heatIndex(90, 60))
 
 
-import aqi  # air quality index - IAQI is intermediate AQI
-
-
-# TOM'S NOTE use karan's api data, replace numbers with data list, loop through and get average
-# Returns aqi result based on pm2.5, pm10 and trioxane using EPA formula (us environment something agency)
-def airQualityCheck():
-    myaqi = aqi.to_iaqi(aqi.POLLUTANT_PM25, '12', algo=aqi.ALGO_EPA)
-    print(myaqi)
-
-    myaqi2 = aqi.to_aqi([
-        (aqi.POLLUTANT_PM25, "12"),
-        (aqi.POLLUTANT_PM10, "24"),
-        (aqi.POLLUTANT_O3_8H, "0.087")  
-    ])
-    return(myaqi2)
-
-
 # Returns temperature changed - change current and previous temp to nasa data list - tbd
 def temperatureChange():
     currentTemperature = 10  #scrape this data
